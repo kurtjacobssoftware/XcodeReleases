ea44e53cc78980c4b28e8744c7cc93b719b02e62
Kurt Jacobs <kurtjacobssoftware@gmail.com>
2025-08-16 02:31:10 +0200

    Initial Commit

---
+++ XcodeReleases/XcodeReleasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeReleasesApp.swift
+//  XcodeReleases
+//
+//  Created by Kurt Jacobs on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct XcodeReleasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeReleasesTests/XcodeReleasesTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeReleasesTests.swift
+//  XcodeReleasesTests
+//
+//  Created by Kurt Jacobs on 2025/08/16.
+//
+
+import Testing
+@testable import XcodeReleases
+
+struct XcodeReleasesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
